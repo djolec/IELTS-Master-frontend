787dc9832ac5e5056aa4e216b4ac4794dca13b88
George <george86.dev@gmail.com>
2025-04-02 22:23:53 +0200

    Changed server env variable

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_BASE_URL = "http://localhost:5500";
+const SERVER_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const serverAxios = axios.create({
   baseURL: SERVER_BASE_URL,
